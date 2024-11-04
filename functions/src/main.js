dfd424c79896cd6c7a6cb2cc55c9329649df690d
zana <nzambwe@gmail.com>
2024-11-04 11:35:12 +0200

    key

--- functions/src/main.js
+++ functions/src/main.js
@@ -5,10 +5,12 @@ export default async ({ req, res, log, error }) => {
   // You can use the Appwrite SDK to interact with other services
   // For this example, we're using the Users service
   const client = new Client()
+
+
     .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
     .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-    .setKey(req.headers['x-appwrite-key'] ?? '');
-  const users = new Users(client);
+    .setKey(process.env.MESSAGE_API_KEY);
+    
   const db = new Databases(client);
   try {
 
